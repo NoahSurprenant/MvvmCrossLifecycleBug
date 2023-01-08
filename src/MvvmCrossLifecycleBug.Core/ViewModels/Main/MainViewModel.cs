a76b43ebda2d6ebecd930dcd6b18a282021563e7
Noah Surprenant <noah.surprenant@gmail.com>
2023-01-07 20:55:59 -0500

    Initial project

---
+++ src/MvvmCrossLifecycleBug.Core/ViewModels/Main/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmCrossLifecycleBug.Core.ViewModels.Main
+{
+    public class MainViewModel : BaseViewModel
+    {
+    }
+}
